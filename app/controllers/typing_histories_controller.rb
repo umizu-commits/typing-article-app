b26887dba48478947fe831c216ddadf38f7e7fda
umizu-commits <mission.mizumizu@gmail.com>
2026-05-30 11:54:50 +0900

    fix: 未ログイン時の履歴一覧アクセスをログインページへリダイレクトする

--- app/controllers/typing_histories_controller.rb
+++ app/controllers/typing_histories_controller.rb
@@ -1,10 +1,10 @@
 class TypingHistoriesController < ApplicationController
-    def index
-      # 未ログインなら Pundit::NotAuthorizedError が発生し、user_not_authorized にフォールバック
-      authorize TypingResult, :index?
-      # 現在のユーザーの結果だけを返す
-      @all_typing_results = policy_scope(TypingResult)
-      # ページネーション適用（テーブル表示用）
-      @typing_results = @all_typing_results.order(created_at: :desc).page(params[:page]).per(10)
-    end
+  before_action :authenticate_user!
+  def index
+    authorize TypingResult, :index?
+    # 現在のユーザーの結果だけを返す
+    @all_typing_results = policy_scope(TypingResult)
+    # ページネーション適用（テーブル表示用）
+    @typing_results = @all_typing_results.order(created_at: :desc).page(params[:page]).per(10)
+  end
 end
